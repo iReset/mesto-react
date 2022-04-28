3741d13ed6b31c36fa3ab5f8f0c9e7803e367bce
iReset <Reset2010@yandex.ru>
2022-04-28 04:21:04 +0300

    feat: add card add form

--- scripts/popups.js
+++ scripts/popups.js
@@ -1,10 +1,13 @@
 const editButton = document.querySelector('.profile__edit-button');
-const closeButtons = document.querySelectorAll('.popup__close-button');
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 let nameInput = document.querySelector('.popup__input_type_name');
 let aboutInput = document.querySelector('.popup__input_type_about');
 let nameField = document.querySelector('.profile__name');
 let aboutField = document.querySelector('.profile__about');
+const addButton = document.querySelector('.profile__add-button');
+const popupAddCard = document.querySelector('.popup_type_add-card');
+const closeButtons = document.querySelectorAll('.popup__close-button');
+
 
 // "Открывает" всплывающее окно
 function openPopup(popup) {
@@ -31,8 +34,23 @@ function saveEditProfile(event) {
   closePopup(popupEditProfile);
 }
 
+// "Открывает" окно добавления карточки
+function openPopupAddCard() {
+  const form = popupAddCard.querySelector('.popup__card');
+  form.reset();
+  openPopup(popupAddCard);
+}
+
+// Обрабатывает оправку формы добавления карточки
+function saveAddCard(event) {
+  event.preventDefault();
+  closePopup(popupAddCard);
+}
+
 editButton.addEventListener('click', openPopupEditProfile);
 popupEditProfile.addEventListener('submit', saveEditProfile);
+addButton.addEventListener('click', openPopupAddCard);
+popupAddCard.addEventListener('submit', saveAddCard);
 closeButtons.forEach(button => {
   const popup = button.closest(".popup");
   button.addEventListener('click', () => closePopup(popup));
