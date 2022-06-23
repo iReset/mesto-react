39518bd96a4fbe49d29c1108d6505a1800826090
iReset <Reset2010@yandex.ru>
2022-06-23 10:45:03 +0300

    feat: add PopupWithImage class

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,25 @@
+export default class PopupWithImage extends Popup {
+  constructor(selector, { captionSelector, imageSelector }) {
+    super(selector);
+    this._caption = this._popup.querySelector(captionSelector);
+    this._image = this._popup.querySelector(imageSelector);
+  }
+
+  open({ link, alt = name, name }) {
+    this._image.src = link;
+    this._image.alt = alt;
+    this._caption.textContent = name;
+    super.open();
+  }
+
+  clear() {
+    this._image.src = '';
+    this._image.alt = '';
+    this._caption.textContent = '';
+  }
+
+  close() {
+    super.close();
+    clear();
+  }
+}
