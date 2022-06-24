d7bf125fc547ccab33d3d023d2f5f52b2f38addc
iReset <Reset2010@yandex.ru>
2022-06-24 13:47:09 +0300

    feat(npm): add build and dev scripts

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: { main: './src/pages/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '',
+    },
+    mode: 'development',
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+        open: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+}
