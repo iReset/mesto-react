9431f94f2f3b681448aabe05030761f8b56c7b34
iReset <Reset2010@yandex.ru>
2022-06-25 03:29:51 +0300

    fix(index): path to FormValidator

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Card from '../components/Card.js';
-import FormValidator from './FormValidator.js';
+import FormValidator from '../components/FormValidator.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import Section from '../components/Section.js';
