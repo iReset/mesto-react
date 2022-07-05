ee83091d01eaf43dacceb1a03787e7cd07b89b18
iReset <Reset2010@yandex.ru>
2022-07-05 17:03:50 +0300

    feat(PopupConfirm): add class PopupConfirm

---
+++ src/components/PopupConfirm.js
@@ -0,0 +1,24 @@
+import Popup from './Popup.js';
+
+export default class PopupConfirm extends Popup {
+  constructor(selector, { formSelector }) {
+    super(selector);
+    this._form = this._popup.querySelector(formSelector);
+    this._handleSubmit = null;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', this._submit.bind(this));
+  }
+
+  setHandler(handler) {
+    this._handleSubmit = handler;
+  }
+
+  _submit(event) {
+    event.preventDefault();
+    this._handleSubmit();
+    this.close();
+  }
+}
