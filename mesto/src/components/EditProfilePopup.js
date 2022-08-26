9dc233a5fddc0443714fac3b26c425daa39170e0
iReset <Reset2010@yandex.ru>
2022-08-26 18:58:11 +0300

    feat(EditProfilePopup): add controllable components

--- mesto/src/components/EditProfilePopup.js
+++ mesto/src/components/EditProfilePopup.js
@@ -1,6 +1,20 @@
+import React from 'react';
+
 import PopupWithForm from './PopupWithForm';
 
 function EditProfilePopup(props) {
+  const [name, setName] = React.useState('');
+  const [about, setAbout] = React.useState('');
+
+  function handleNameChange(e) {
+    console.log(e.target.value)
+    setName(e.target.value);
+  }
+
+  function handleAboutChange(e) {
+    setAbout(e.target.value);
+  }
+
   return (
     <PopupWithForm
       name="edit-profile"
@@ -11,11 +25,11 @@ function EditProfilePopup(props) {
       onClose={props.onClose}
     >
       <fieldset className="popup__fieldset">
-        <input className="popup__input popup__input_type_name" type="text" id="name" name="name" value=""
-          placeholder="Имя" required minLength="2" maxLength="40" />
+        <input className="popup__input popup__input_type_name" type="text" id="name" name="name" value={name}
+          placeholder="Имя" required minLength="2" maxLength="40" onChange={handleNameChange} />
         <span className="popup__input-error" id="name-error" hidden></span>
-        <input className="popup__input popup__input_type_about" type="text" id="about" name="about" value=""
-          placeholder="О себе" required minLength="2" maxLength="200" />
+        <input className="popup__input popup__input_type_about" type="text" id="about" name="about" value={about}
+          placeholder="О себе" required minLength="2" maxLength="200" onChange={handleAboutChange} />
         <span className="popup__input-error" id="about-error" hidden></span>
       </fieldset>
     </PopupWithForm>
